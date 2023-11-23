dee91f10b475ee3eb03ccac8b65381fa48f60d2d
anasalazmeh <anoosalazmeh@gmail.com>
2023-11-23 19:20:01 +0300

    style the active link in the navber

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,10 +1,14 @@
+'use client'
+import classNames from "classnames";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 import { AiFillBug } from "react-icons/ai";
 export const Navbar = () => {
+  const currentPath = usePathname();
   const link = [
     { label: "Dashbored", href: "/" },
-    { label: "Issue", href: "/" },
+    { label: "Issue", href: "/issue" },
   ];
   return (
     <nav className="flex px-5 mb-5 space-x-6 h-14 items-center border-b-2">
@@ -15,7 +19,12 @@ export const Navbar = () => {
         {link.map((link) => (
           <li
             key={link.href}
-            className="text-zinc-500 hover:text-zinc-800 transition-colors"
+            // className="text-zinc-500 hover:text-zinc-800 transition-colors"
+            className={classNames({
+              "text-zinc-900": link.href == currentPath,
+              "text-zinc-500": link.href != currentPath,
+              'hover:text-zinc-800 transition-colors': true,
+            })}
           >
             <Link href={link.href}>{link.label}</Link>
           </li>
