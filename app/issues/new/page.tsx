890efc023d9048443acd8b91a0c87469b8352e7f
anasalazmeh <anoosalazmeh@gmail.com>
2023-11-24 17:29:00 +0300

    Refactor: Move inline  function

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -10,44 +10,54 @@ import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { Controller, useForm } from "react-hook-form";
 import SimpleMDE from "react-simplemde-editor";
-import { z } from 'zod';
-type Issueform =z.infer<typeof createissueshema>
-const  NewIssues = () => {
-  const route=useRouter()
-  const { control, handleSubmit, register,formState:{errors} } = useForm<Issueform>(
-    {resolver:zodResolver(createissueshema)}
-  );
-  const [error,seterror]=useState('')
-  const [isSunbit,setSumbit]=useState(false)
+import { z } from "zod";
+type Issueform = z.infer<typeof createissueshema>;
+const NewIssues = () => {
+  const route = useRouter();
+  const {
+    control,
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm<Issueform>({ resolver: zodResolver(createissueshema) });
+  const [error, seterror] = useState("");
+  const [isSunbit, setSumbit] = useState(false);
+  const Submit = handleSubmit(async (data) => {
+    try {
+      setSumbit(true);
+      await axios.post("/api/issues", data);
+      setSumbit(false);
+      route.push("/issues");
+    } catch (error) {
+      seterror("An unexpevted error occurred.");
+      route.push("/issues/new");
+    }
+  });
+
   return (
     <div className="max-w-xl">
-      {error && <Callout.Root className="mb-5" color="red"><CalloutText >{error}</CalloutText></Callout.Root>}
-      <form className="space-y-3  " onSubmit={handleSubmit(async (data)=>{
-       try{
-        setSumbit(true)
-        await axios.post('/api/issues',data)
-        setSumbit(false)
-        route.push('/issues')
-       }
-       catch (error){
-        seterror('An unexpevted error occurred.')
-        route.push('/issues/new')
-       }
-    })}>
-      <TextField.Root>
-        <TextField.Input placeholder="Title" {...register("title")} />
-      </TextField.Root>
+      {error && (
+        <Callout.Root className="mb-5" color="red">
+          <CalloutText>{error}</CalloutText>
+        </Callout.Root>
+      )}
+      <form className="space-y-3  " onSubmit={Submit}>
+        <TextField.Root>
+          <TextField.Input placeholder="Title" {...register("title")} />
+        </TextField.Root>
         <ErrorMessage>{errors.title?.message}</ErrorMessage>
-      <Controller
-        name="description"
-        control={control}
-        render={({ field }) => (
-          <SimpleMDE placeholder="Description" {...field} />
-        )}
-      />
-       <ErrorMessage>{errors.description?.message}</ErrorMessage>
-      <Button className="disabled:bg-violet-600" disabled={isSunbit}>Submit new issue {isSunbit && <Spinner/>}</Button>
-    </form>
+        <Controller
+          name="description"
+          control={control}
+          render={({ field }) => (
+            <SimpleMDE placeholder="Description" {...field} />
+          )}
+        />
+        <ErrorMessage>{errors.description?.message}</ErrorMessage>
+        <Button className="disabled:bg-violet-600" disabled={isSunbit}>
+          Submit new issue {isSunbit && <Spinner />}
+        </Button>
+      </form>
     </div>
   );
 };
