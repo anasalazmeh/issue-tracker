f467c9ea66eeb3888a5146c06b1b6306db26f5b2
anasalazmeh <anoosalazmeh@gmail.com>
2023-11-27 21:22:51 +0300

    Fix avatar if loading

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -45,6 +45,7 @@ export const Navbar = () => {
                 size="3"
                 radius="full"
                 className="cursor-pointer"
+                referrerPolicy="no-referrer"
               />
             </DropdownMenu.Trigger>
             <DropdownMenu.Content>
