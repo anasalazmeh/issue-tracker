10b84253aad265bc58fbd2e0fdc69cee5f06fa79
anasalazmeh <anoosalazmeh@gmail.com>
2023-11-28 21:17:35 +0300

    Enhance rhe API to assign issues

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,46 +1,59 @@
-
 import authOptions from "@/app/auth/authOptions";
-import { issueshema } from "@/app/validationSachema";
+import { issueshema, patchissueschema } from "@/app/validationSachema";
 import prisma from "@/prisma/client";
 import delay from "delay";
 import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function PATCH(request: NextRequest,{params}:{params:{id:string}}) {
-  const session =getServerSession(authOptions)
-  if(!session)
-  return NextResponse.json({},{status:401})
+export async function PATCH(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  // const session =getServerSession(authOptions)
+  // if(!session)
+  // return NextResponse.json({},{status:401})
   const body = await request.json();
-  const validation = issueshema.safeParse(body); 
+  const validation = patchissueschema.safeParse(body);
   if (!validation.success)
-    return NextResponse.json(validation.error.errors, { status: 400 });
-  const issue=await prisma.issue.findUnique({
-    where:{id:parseInt(params.id)}
-  })
-  if(!issue)
-  return NextResponse.json({error:"Invalied issue"},{status:404})
+    return NextResponse.json(validation.error.format(), { status: 400 });
+  const { title, description, assignedToUserId } = body;
+  if(assignedToUserId){
+    const user = await prisma.user.findUnique({
+      where: { id: assignedToUserId },
+    });
+    if (!user)
+    return NextResponse.json({ error: "Invalied user." }, { status: 400 });
+  }
+  const issue = await prisma.issue.findUnique({
+    where: { id: parseInt(params.id) },
+  });
+  if (!issue)
+    return NextResponse.json({ error: "Invalied issue" }, { status: 404 });
   const newIssue = await prisma.issue.update({
-    where:{id:issue.id},
+    where: {id:issue.id},
     data: {
-      title: body.title,
-      description: body.description,
+      title,
+      description,
+      assignedToUserId,
     },
   });
   return NextResponse.json(newIssue, { status: 201 });
 }
 
-export async function DELETE(request: NextRequest,{params}:{params:{id:string}}) {
-  const session =getServerSession(authOptions)
-  if(!session)
-  return NextResponse.json({},{status:401})
-  const issue=await prisma.issue.findUnique({
-    where:{id:parseInt(params.id)}
-  })
-  if(!issue)
-  return NextResponse.json({error:"Invalied issue"},{status:404})
- await delay(2000)
+export async function DELETE(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const session = getServerSession(authOptions);
+  if (!session) return NextResponse.json({}, { status: 401 });
+  const issue = await prisma.issue.findUnique({
+    where: { id: parseInt(params.id) },
+  });
+  if (!issue)
+    return NextResponse.json({ error: "Invalied issue" }, { status: 404 });
+  await delay(2000);
   const newIssue = await prisma.issue.delete({
-    where:{id:issue.id},
+    where: { id: issue.id },
   });
   return NextResponse.json(newIssue, { status: 201 });
 }
--- app/validationSachema.ts
+++ app/validationSachema.ts
@@ -1,6 +1,20 @@
 import { z } from "zod";
 
 export const issueshema = z.object({
-  title: z.string().min(1, "Title is requitrd").max(255),
-  description: z.string().min(1, "Description is requitrd"),
+  title: z.string().min(1, "Title is requirrd1.").max(255),
+  description: z.string().min(1, "Description is requirrd."),
+});
+export const patchissueschema = z.object({
+  title: z.string().min(1,"Title is required.").max(255).optional(),
+  description: z
+    .string()
+    .min(1, "Description is requirrd.")
+    .max(65535)
+    .optional(),
+    assignedToUserId: z
+    .string()
+    .min(1, "AssignedToUserId is required.")
+    .max(255)
+    .optional()
+    .nullable(),
 });
