9cd654322823b9c9f9cac175b098c28876970644
anasalazmeh <anoosalazmeh@gmail.com>
2023-11-29 19:46:34 +0300

    implement pageination

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Pagniation from "./components/Pagniation";
 
-export default function Home() {
+export default function Home({searchParams}:{searchParams:{page:string}}) {
   return (
-    <Pagniation itemCount={100} pageSize={20} currentPage={1}/>
+    <Pagniation itemCount={100} pageSize={20} currentPage={parseInt(searchParams.page)|| 1}/>
   )
 }
